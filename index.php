854ba41453982150af10ce68a7db56d6c1c94e9f
Corey Edwards <cedwardsmedia@gmail.com>
2015-06-08 17:21:51 -0500

    Added require 'vendor/autoload.php'

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
 error_reporting(E_ALL);
 
+require 'vendor/autoload.php';
+
 // Account SID
 $SID="AC2877277cd21a44af8e9344c53bb13119";
 // AUTH Token
 $TOKEN="AUe03f0c1796c04eed8d838061895b4534";
 
 function init() {
-   require __DIR__ . '/vendor/autoload.php';
+
 }
 
 function apicall($SID, $TOKEN, $phone) {
