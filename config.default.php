9a9369882b0dbb6c33662581af64391ae627c1b5
Corey Edwards <corey@cedwardsmedia.com>
2015-10-23 03:24:34 -0500

    Added default config

---
+++ config.default.php
@@ -0,0 +1,14 @@
+<?php
+// Everyone API Credentials
+
+// Account SID
+$SID = "";
+// AUTH Token
+$TOKEN = "";
+
+// cURL binary to use (if different from system default)
+$curl = "";
+
+// Do not edit below this line.
+
+?>
