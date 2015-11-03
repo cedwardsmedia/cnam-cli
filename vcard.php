b3cd09fa40955285a9b46ba4ffba3f8c7a9e6e7f
Corey Edwards <corey@cedwardsmedia.com>
2015-11-02 22:14:35 -0600

    Added Revision Field

--- vcard.php
+++ vcard.php
@@ -2,6 +2,13 @@
    session_start();
    header("Content-Type: text/vcard; charset=utf-8");
    header('Content-Disposition: attachment; filename="' . $_SESSION['lastname'] . '.vcf');
+
+   // Check php.ini for timezone. If it's not set, default to UTC
+
+   $timezone = ini_get('date.timezone');
+   if (!$timezone) {
+   date_default_timezone_set("UTC");
+   };
 ?>
 BEGIN:VCARD
 VERSION:3.0
@@ -14,4 +21,6 @@ PHOTO;TYPE=JPEG:http:<?php echo $_SESSION['image']; ?>
 TEL;TYPE=<?php if ($_SESSION['linetype'] == "mobile") { echo "CELL"; } else { echo "HOME";}; ?>,type=VOICE;type=pref:+1<?php echo $_SESSION['phone']; ?>
 
 ADR;type=HOME:;;;<?php echo $_SESSION['city']; ?>;<?php echo $_SESSION['state']; ?>;<?php echo $_SESSION['zip']; ?>;United States
+REV:<?php echo date('Y-m-d') . "T" . date('H:i:s'); ?>
+
 END:VCARD
