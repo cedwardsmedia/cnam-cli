d3846da7a69e77e6ab0fc3fc722e2ea977251d67
Corey Edwards <corey@cedwardsmedia.com>
2015-11-03 21:50:27 -0600

    Moved main.css to assets/css

--- index.php
+++ index.php
@@ -136,7 +136,7 @@
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 
     <!-- Custom styles for this template -->
-    <link href="main.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
