a864017fc1c724d5ec8d4052c6c94d49fe7cf703
Corey Edwards <corey@cedwardsmedia.com>
2015-11-03 22:37:47 -0600

    Require config.php

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -1,15 +1,18 @@
 <?php
 // Start the session so we can save
 // query results across pages requests.
+
+
 session_start();
 
 ini_set('display_errors', 0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 require 'vendor/autoload.php';
-require("functions.inc.php");
+require 'functions.inc.php';
+include 'config.php';
+
 
 define("APPNAME", "CNAM");
 define("VERSION", "1.2");
 define("DEVELOPER", "Corey Edwards");
-define("GITHUB", "");
