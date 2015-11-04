3e691707073927a9f4228c744d21c791f2af7460
Corey Edwards <corey@cedwardsmedia.com>
2015-11-03 22:50:54 -0600

    Renamed functions.inc.php

--- APICaller.php
+++ APICaller.php
@@ -1,5 +1,5 @@
 <?php
-class ApiCaller {
+class APICaller {
 
    public $http;
    public $error;
--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -9,10 +9,13 @@ ini_set('display_errors', 0);
 error_reporting(E_ALL & ~E_NOTICE);
 
 require 'vendor/autoload.php';
-require 'functions.inc.php';
+require 'APICaller.php';
 include 'config.php';
 
 
 define("APPNAME", "CNAM");
 define("VERSION", "1.2");
+define("DEVGITHUB", "https://www.github.com/cedwardsmedia/cnam");
 define("DEVELOPER", "Corey Edwards");
+define("COPYRIGHTYEAR", "2014 - 2015");
+define("COPYRIGHT", "&copy; " . COPYRIGHTYEAR . " " . DEVELOPER);
