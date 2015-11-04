57468301c89596406ae41a2d69eb4fd347af6c0f
Corey Edwards <corey@cedwardsmedia.com>
2015-11-03 23:22:23 -0600

    Updated status messages

---
+++ eapiauth.php
@@ -0,0 +1,37 @@
+<?php
+
+ini_set('display_errors', 0);
+error_reporting(E_ALL & ~E_NOTICE);
+
+$remember_me = $_GET['rememberme'];
+
+$sid = $_POST['sid'];
+$token = $_POST['token'];
+
+if ($sid && $token) {
+    if ($remember_me) {
+        /* Set cookie to last 1 year */
+        setcookie('sid', $sid, 60 * 60 * 24 * 365);
+        setcookie('token', $token, 60 * 60 * 24 * 365);
+    } else {
+        /* Cookie expires when browser closes */
+        setcookie('sid', $sid, false);
+        setcookie('token', $token, false);
+    }
+
+    echo json_encode([
+        'status' => 'ok',
+        'payload' => [
+            'message' => '<i class="fa fa-check-circle"></i> Credentials saved!'
+        ]
+    ]);
+    exit;
+} else {
+    echo json_encode([
+        'status' => 'error',
+        'payload' => [
+            'message' => '<i class="fa fa-warning"></i> Oops, something went wrong.'
+        ]
+    ]);
+    exit;
+}
