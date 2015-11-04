d84ea978b71e0947e1d40f78a65020c789898ed0
Corey Edwards <corey@cedwardsmedia.com>
2015-11-04 15:07:40 -0600

    Added API error handler

--- cnam.php
+++ cnam.php
@@ -24,45 +24,54 @@ error_reporting(E_ALL & ~E_NOTICE);
             $api = new APICaller();
             $api->api_call($phone);
 
+
+            // Check for errors reported by API
+
+            // Print any error and die
+            if ($api->error) {
+               print_r("Error: $api->error\n");
+               exit(2);
+            } else {
+
             // Print Dossier
-            hr();
-            print_r($api->data->data->cnam . "\n");
-            hr();
-            // Print Name
-            if ($api->data->data->gender == "M"){$salutation = "Mr.";} elseif ($api->data->data->gender == "F"){$salutation = "Ms.";};
-            print_r("Name:\n   " . $salutation ." " . $api->data->data->expanded_name->first . " " . $api->data->data->expanded_name->last ."\n\n");
+               hr();
+               print_r($api->data->data->cnam . "\n");
+               hr();
+               // Print Name
+               if ($api->data->data->gender == "M"){$salutation = "Mr.";} elseif ($api->data->data->gender == "F"){$salutation = "Ms.";};
+               print_r("Name:\n   " . $salutation ." " . $api->data->data->expanded_name->first . " " . $api->data->data->expanded_name->last ."\n\n");
 
-            // Print Address
-            print_r("Address:\n   " . $api->data->data->address. "\n   " . $api->data->data->location->city . ", " . $api->data->data->location->state . " " . $api->data->data->location->zip . "\n\n");
+               // Print Address
+               print_r("Address:\n   " . $api->data->data->address. "\n   " . $api->data->data->location->city . ", " . $api->data->data->location->state . " " . $api->data->data->location->zip . "\n\n");
 
-            // Print Gender
-            echo("Gender: ");
-            if ($api->data->data->gender == "M"){echo "Male";} elseif ($api->data->data->gender == "F"){echo "Female";};
-            echo "\n\n";
+               // Print Gender
+               echo("Gender: ");
+               if ($api->data->data->gender == "M"){echo "Male";} elseif ($api->data->data->gender == "F"){echo "Female";};
+               echo "\n\n";
 
-            // Print Relationship
-            print_r("Relationship:\n   " . $api->data->data->profile->relationship . "\n\n");
+               // Print Relationship
+               print_r("Relationship:\n   " . $api->data->data->profile->relationship . "\n\n");
 
-            // Print Image
-            print_r("Image:\n   http:" . $api->data->data->image->large . "\n\n");
+               // Print Image
+               print_r("Image:\n   http:" . $api->data->data->image->large . "\n\n");
 
-            // Print Job
-            print_r("Job:\n   " . $api->data->data->profile->job . "\n\n");
+               // Print Job
+               print_r("Job:\n   " . $api->data->data->profile->job . "\n\n");
 
-            // Print Education
-            print_r("Edu:\n   " . $api->data->data->profile->edu . "\n\n");
+               // Print Education
+               print_r("Edu:\n   " . $api->data->data->profile->edu . "\n\n");
 
-            // Print Linetype
-            print_r("Linetype:\n   " . $api->data->data->linetype . "\n\n");
+               // Print Linetype
+               print_r("Linetype:\n   " . $api->data->data->linetype . "\n\n");
 
-            // Print Original Carrier
-            print_r("Original Carrier:\n   " . $api->data->data->carrier_o->name . "\n\n");
+               // Print Original Carrier
+               print_r("Original Carrier:\n   " . $api->data->data->carrier_o->name . "\n\n");
 
-            // Print Current Carrier
-            print_r("Current Carrier:\n   " . $api->data->data->carrier->name . "\n\n");
+               // Print Current Carrier
+               print_r("Current Carrier:\n   " . $api->data->data->carrier->name . "\n\n");
 
-            hr();
-
+               hr();
+            }
       }
    } else {
       usage();
