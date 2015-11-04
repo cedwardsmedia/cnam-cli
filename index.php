cd19d83fa500ad2be190f9a86b6936967c14e3c9
Corey Edwards <corey@cedwardsmedia.com>
2015-11-03 23:22:52 -0600

    Added meta description

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <meta name="description" content="">
+    <meta name="description" content=" Web-based reverse phone number lookup tool written in PHP and sourced by EveryoneAPI.">
     <meta name="author" content="Corey Edwards">
     <link rel="apple-touch-icon" sizes="57x57" href="assets/icons/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="assets/icons/apple-icon-60x60.png">
