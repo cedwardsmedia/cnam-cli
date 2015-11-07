88909968181b05152b46cfdf1de95ecbc4663203
Corey Edwards <corey@cedwardsmedia.com>
2015-11-06 22:39:20 -0600

    Added PHP version check

--- bootstrap.inc.php
+++ bootstrap.inc.php
@@ -2,7 +2,6 @@
 // Start the session so we can save
 // query results across pages requests.
 
-
 session_start();
 
 ini_set('display_errors', 0);
@@ -20,3 +19,9 @@ define("DEVGITHUB", "https://www.github.com/cedwardsmedia/cnam");
 define("DEVELOPER", "Corey Edwards");
 define("COPYRIGHTYEAR", "2014 - 2015");
 define("COPYRIGHT", "&copy; " . COPYRIGHTYEAR . " " . DEVELOPER);
+
+// Check which version of PHP we're using. If it's too old, die.
+if (phpversion() < "5.5") {
+   echo(APPNAME . " requires PHP 5.5 or greater. You are currently running PHP " . phpversion() . ". Please upgrade PHP.\n");
+   exit(1);
+   }
